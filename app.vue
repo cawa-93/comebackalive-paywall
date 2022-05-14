6389f1332dd36fec772f81b396a11df6dfde7550
Alex Kozack <cawa-93@users.noreply.github.com>
2022-05-14 13:20:46 +0300

    Покращено підказки до полів

--- app.vue
+++ app.vue
@@ -97,13 +97,16 @@ const reportUrl = computed(() => {
       <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Адреса призначення</label>
         <input type="url" v-model="next" class="form-control" required placeholder="https://…">
-        <div id="emailHelp" class="form-text">На цю адресу буде переспрямовано людину після донату</div>
+        <div id="emailHelp" class="form-text">
+          На цю адресу буде переспрямовано людину після донату. Це може бути посилання на відео, арт, статтю, архів файлів будь-де, закриту групу тощо.
+          Швидко створити сторінку в інтернеті можна тут: <a href="https://telegra.ph/" target="_blank">telegra.ph</a>
+        </div>
       </div>
 
       <div class="mb-3">
         <label for="exampleFormControlInput1" class="form-label">Унікальний тег</label>
         <input type="search" v-model="tag" class="form-control" required>
-        <div id="emailHelp" class="form-text">Вкажіть тег для цього посилання. Слугує для побудови звітів</div>
+        <div id="emailHelp" class="form-text">Слугує для побудови звітів. Це може бути будь який текст. Всі донати з однаковим тегом будуть об’єднані в один звіт.</div>
       </div>
 
       <div class="mb-3">
@@ -118,33 +121,33 @@ const reportUrl = computed(() => {
         </div>
       </div>
 
+      <hr>
+
+      <div class="mb-3">
+        <label for="staticEmail" class="form-label">Посилання на збір </label>
+        <div class="input-group">
+          <input type="url" readonly class="form-control" id="staticEmail" :value="donationUrl">
+          <a target="_blank" v-if="donationUrl" :href="donationUrl" class="input-group-text">🔗</a>
+        </div>
+        <div id="emailHelp" class="form-text">
+          <b>Публікуйте це посилання в соц.мережах</b>.
+          Перейшовши за ним, відвідувач спочатку внесе донат і після зарахування коштів буде переспрямований на адресу призначення
+        </div>
+      </div>
+
+
+      <div class="mb-3">
+        <label for="staticEmail" class="form-label">Звіт всіх донатів за тегом тут</label>
+        <div class="input-group">
+          <input type="url" readonly class="form-control" id="staticEmail" :value="reportUrl">
+          <a target="_blank" v-if="reportUrl" :href="reportUrl" class="input-group-text">🔗</a>
+        </div>
+        <div id="emailHelp" class="form-text">
+          <b>Цей URL для вас</b>.
+          За ним ви можете подивитись загальну суму донатів за тегом.
+        </div>
+      </div>
     </form>
-
-    <div class="mb-3">
-      <label for="staticEmail" class="form-label">Посилання на збір </label>
-      <div class="input-group">
-        <input type="url" readonly class="form-control" id="staticEmail" :value="donationUrl">
-        <a target="_blank" v-if="donationUrl" :href="donationUrl" class="input-group-text">🔗</a>
-      </div>
-      <div id="emailHelp" class="form-text">
-        <b>Публікуйте це посилання в соц.мережах</b>.
-        Перейшовши за ним, відвідувач спочатку внесе донат і після зарахування коштів буде переспрямований на адресу призначення
-      </div>
-    </div>
-
-
-    <div class="mb-3">
-      <label for="staticEmail" class="form-label">Звіт всіх донатів за тегом тут</label>
-      <div class="input-group">
-        <input type="url" readonly class="form-control" id="staticEmail" :value="reportUrl">
-        <a target="_blank" v-if="reportUrl" :href="reportUrl" class="input-group-text">🔗</a>
-      </div>
-      <div id="emailHelp" class="form-text">
-        <b>Цей URL для вас</b>.
-        За ним ви можете подивитись загальну суму донатів за тегом.
-      </div>
-    </div>
-
   </div>
 </template>
 
